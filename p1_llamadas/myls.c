49f6fa744f329eeb83426d012c75bbf267c4d35a
torrescar <cmt57@duke.edu>
2017-02-27 14:55:48 +0100

    final-ish files

--- p1_llamadas/myls.c
+++ p1_llamadas/myls.c
@@ -11,6 +11,7 @@ int main(int argc, char *argv[])
 	struct dirent *dp;
 	char buf[PATH_MAX];
 
+    /* If no arguments are passed when main is run, program tries to open the current directory for reading and sets the directory pointer, dirp, to the pointer to the directory stream for current directory.  However, if current directory cannot be opened, its contents cannot be read, so it prints the opening error and program returns -1 */
 	if(argc < 2)
 	{
 		if((dirp=opendir(getcwd(buf, PATH_MAX))) == NULL) {
@@ -18,6 +19,7 @@ int main(int argc, char *argv[])
 			return -1;
 		}
 	}
+    /* If an arguments IS specified when main is run, program tries to open the first directory specified for reading and sets the directory pointer, dirp, to the pointer to the directory stream for that specified directory.  However, if the directory specified cannot be opened (including if it is not accessible or not actually a directory in existence or in type), its contents cannot be read, so it prints the opening error and program returns -1 */
 	else {
 		if((dirp=opendir(argv[1])) == NULL) {
 			perror("couldn't open directory");
@@ -25,15 +27,14 @@ int main(int argc, char *argv[])
 		}
 	}
 	
-	
-	  while (dirp) {
-   		 if ((dp = readdir(dirp)) != NULL) {
-        		printf("%s\n",dp->d_name);
-      		 } else {
-         		closedir(dirp);
-         		return -1;
-      		 }
-         }
- 	 return 0;
+    /* At this point, there is definitely a directory specified that can be opened, so program tries to read it (referenced by dirp).  If program cannot read its entries, there is no next directory entry to be pointed to by dirent pointer, dp, so dp is set to NULL and the directory stream, dirp, is closed and program returns -1 because success is never set to 0 if readdir(dirp) never worked.  Otherwise, 0 is returned because dp at some point had a next entry to list since readdir(dirp) worked */
+    int success = -1;
+    while ((dp = readdir(dirp)) != NULL) {
+        success = 0;
+        printf("%s\n",dp->d_name);
+    }
+    
+    closedir(dirp);
+    return success;
 }
 
--- p1_llamadas/mysize.c
+++ p1_llamadas/mysize.c
@@ -19,28 +19,24 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 	
-	while (dirp) {
-   		if ((dp = readdir(dirp)) != NULL){  
-			if (dp->d_type != DT_REG)
-				continue;		
-			if ( (fd= open(dp->d_name, O_RDONLY, 0666))<0)
- 		  	{
-    				perror("Error creating the file");
-    				return -1;
-  			}
-			
+    int success = -1;
+	while ((dp = readdir(dirp)) != NULL) {
+        success = 0;
+        if (dp->d_type != DT_REG)
+            continue;
+        if ( (fd= open(dp->d_name, O_RDONLY, 0666))<0) {
+            perror("Error opening the file");
+            return -1;
+        }
+        
+        if((fileSize=lseek(fd,0,SEEK_END)) < 0) {
+            perror("seek error");
+            return -1;
+        }
+        close(fd);
+        printf("%s\t%d\n",dp->d_name,fileSize);
 
-		        if((fileSize=lseek(fd,0,SEEK_END)) < 0) {
-
-     				perror("seek error");
-     				return -1;
-  			}
-        		printf("%s\t%d\n",dp->d_name,fileSize);
-			
-      		} else {
-         		closedir(dirp);
-         		return -1;
-      		}
 	}
- 	return 0;
+    closedir(dirp);
+ 	return success;
 }
